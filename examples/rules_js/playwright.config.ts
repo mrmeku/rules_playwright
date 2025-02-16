a97474818cf6dbc184ebaa9e7b30dff0682332cc
Daniel Muller <mrmeku@gmail.com>
2025-02-16 13:58:58 -0700

    Add todomvc tests and UI target

--- examples/rules_js/playwright.config.ts
+++ examples/rules_js/playwright.config.ts
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig, devices } from "@playwright/test";
 
+console.log(process.env.PWD, process.env.TEST_SERVER);
+
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   testDir: "./tests",
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -15,13 +18,33 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? "list" : "html",
+  // globalSetup: "./global-setup",
+  webServer: {
+    command: `./${process.env.TEST_SERVER}`,
+    port: 1234,
+    reuseExistingServer: false,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
 });
